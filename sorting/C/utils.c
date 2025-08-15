1f66411233184a867faa56e06dc6b4750f0575a5
Alessandro Nicolini <alessandronicolini395@gmail.com>
2025-08-15 19:51:59 +0200

    [UPDATE] update size_t to int in printing function

--- sorting/C/utils.c
+++ sorting/C/utils.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "utils.h"
 
-void print_array(int *arr, size_t length) {
+void print_array(int *arr, int length) {
     printf("arr[");
     for (int i = 0; i < length - 1; i++) {
         printf("%d, ", arr[i]);
--- sorting/C/utils.h
+++ sorting/C/utils.h
@@ -1,5 +1,5 @@
 #ifndef UTILS_H
 #define UTILS_H
 
-void print_array(int *arr, size_t length);
+void print_array(int *arr, int length);
 #endif
