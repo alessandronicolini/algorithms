0f276809661dd89ff8227e9a390199a35ce3aff7
Alessandro Nicolini <alessandronicolini395@gmail.com>
2025-08-15 20:25:33 +0200

    [FEATURE] Adds sorting order to insertion-sort py version

--- sorting/python/insertion-sort.py
+++ sorting/python/insertion-sort.py
@@ -1,13 +1,38 @@
 from typing import List
+from enum import Enum, unique, auto
 
-def insertionSort(arr: List[int]):
+
+@unique
+class Order(Enum):
+    ASC = auto()
+    DESC = auto()
+
+
+def insertion_sort(arr: List[int], ord: Order):
     for j in range(1, len(arr)):
+        
         key = arr[j]
         i = j - 1
-        while i >= 0 and arr[i] > key:
-            arr[i + 1] = arr[i]
-            i = i - 1
+
+        if ord == Order.ASC:
+            while i >= 0 and arr[i] > key:
+                arr[i + 1] = arr[i]
+                i = i - 1
+        else:
+            while i >= 0 and arr[i] < key:
+                arr[i + 1] = arr[i]
+                i = i - 1
+
         arr[i + 1] = key
+
+
+def insertion_sort_asc(arr: List[int]):
+    insertion_sort(arr, Order.ASC)
+
+
+def insertion_sort_desc(arr: List[int]):
+    insertion_sort(arr, Order.DESC)
+
 
 if __name__ == "__main__":
 
@@ -16,6 +41,11 @@ if __name__ == "__main__":
     print(f"arr{arr}")
     print()
 
-    insertionSort(arr)
-    print("INPUT SORTED")
+    insertion_sort_asc(arr)
+    print("INPUT SORTED (ASC order)")
+    print(f"arr{arr}")
+    print()
+
+    insertion_sort_desc(arr)
+    print("INPUT SORTED (DESC order)")
     print(f"arr{arr}")
