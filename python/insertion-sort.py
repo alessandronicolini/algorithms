1624cff158ee3de4516c671caa03fcf6aeaec759
Alessandro Nicolini <alessandronicolini395@gmail.com>
2025-08-16 21:30:35 +0200

    [REFACTOR python] define comparator function
    Comparator function can be defines at the beginning according the Order value

--- python/insertion-sort.py
+++ python/insertion-sort.py
@@ -1,6 +1,6 @@
 from typing import List
 from enum import Enum, unique, auto
-
+import operator
 
 @unique
 class Order(Enum):
@@ -8,44 +8,37 @@ class Order(Enum):
     DESC = auto()
 
 
-def insertion_sort(arr: List[int], ord: Order):
+def insertion_sort(arr: List[int], ord = Order.ASC):
+    
+    if ord == Order.ASC:
+        comp = operator.gt
+    else:
+        comp = operator.lt
+
     for j in range(1, len(arr)):
         
         key = arr[j]
         i = j - 1
 
-        if ord == Order.ASC:
-            while i >= 0 and arr[i] > key:
-                arr[i + 1] = arr[i]
-                i = i - 1
-        else:
-            while i >= 0 and arr[i] < key:
-                arr[i + 1] = arr[i]
-                i = i - 1
+        while i >= 0 and comp(arr[i], key):
+            arr[i + 1] = arr[i]
+            i = i - 1
 
         arr[i + 1] = key
 
 
-def insertion_sort_asc(arr: List[int]):
-    insertion_sort(arr, Order.ASC)
-
-
-def insertion_sort_desc(arr: List[int]):
-    insertion_sort(arr, Order.DESC)
-
-
 if __name__ == "__main__":
 
-    arr = [5, 4, 3, 2]
+    arr = [2, 4, 5, 9, 2, 10, -1, 8]
     print("PROBLEM INPUT")
     print(f"arr{arr}")
     print()
 
-    insertion_sort_asc(arr)
+    insertion_sort(arr)
     print("INPUT SORTED (ASC order)")
     print(f"arr{arr}")
     print()
 
-    insertion_sort_desc(arr)
+    insertion_sort(arr, Order.DESC)
     print("INPUT SORTED (DESC order)")
     print(f"arr{arr}")
