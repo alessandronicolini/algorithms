b9b7550537f75ce5a852a8408ca511ea9ee13f86
Alessandro Nicolini <alessandronicolini395@gmail.com>
2025-08-18 20:23:31 +0200

    refactor(C-insertion_sort): cleans ascending / descending sorting order feature

--- C/selection-sort.c
+++ C/selection-sort.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include "utils.h"
+#include <stdbool.h>
 
 #define N 8
 
-void selection_sort(int *arr, int n, Order ord) {
+void selection_sort(int *arr, int n, bool ascending) {
 
     int m_value, m_pos, temp;
 
@@ -16,21 +17,16 @@ void selection_sort(int *arr, int n, Order ord) {
         
         // loop over the right unordered subarray and find the m (min or max)
         // according the sorting order
-        if (ord == ORDER_ASC) {
-            for (int j = i + 1; j < n; j++) {
-                if (arr[j] < m_value) {
-                    m_value = arr[j];
-                    m_pos = j;
-                }
+
+        for (int j = i + 1; j < n; j++) {
+            if (
+                (ascending && arr[j] < m_value) ||
+                (!ascending && arr[j] >= m_value)
+            ) {
+                m_value = arr[j];
+                m_pos = j;
             }
-        } else {
-            for (int j = i + 1; j < n; j++) {
-                if (arr[j] > m_value) {
-                    m_value = arr[j];
-                    m_pos = j;
-                }
-            }
-        }
+        } 
         
         // exchange the found m value with the first element of
         // the current right unordered subarray.
@@ -43,17 +39,20 @@ void selection_sort(int *arr, int n, Order ord) {
 }
 
 int main(void) {
+    bool asc_order = true;
+    bool desc_order = !asc_order;
+
     int arr[N] = {3, 5, 2, 7, 8, -2, -5, 6};
     printf("PROBLEM INPUT:\n");
     print_array(arr, N);
     printf("\n");
 
-    selection_sort(arr, N, ORDER_ASC);
+    selection_sort(arr, N, asc_order);
     printf("SORTED (ASC order):\n");
     print_array(arr, N);
     printf("\n");
 
-    selection_sort(arr, N, ORDER_DESC);
+    selection_sort(arr, N, desc_order);
     printf("SORTED (DESC order):\n");
     print_array(arr, N);
 }
